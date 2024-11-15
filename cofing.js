0ba1442bd3ef5e9d05a1313ee68fdf7ef9b8be11
pelapato <129638566+pelapato@users.noreply.github.com>
2024-11-15 16:25:05 +0000

    keyboard version 1.6

--- cofing.js
+++ cofing.js
@@ -9,6 +9,9 @@ const texto=document.querySelector("#saida-de-texto-aqui")
 //Os butoes que sao letras
 const botao = [...document.querySelectorAll("button:not(#upper):not(#search):not(#numero):not(#space):not(#delete)")]
 
+const coluna_um = [...document.querySelectorAll("#coluna-1 > button")]
+const coluna_dois = [...document.querySelectorAll("#coluna-2 > button")]
+const coluna_tres = [...document.querySelectorAll("#coluna-3 > button")]
 
 //botoes especiais
 const exit = document.querySelector("#delete")
@@ -20,6 +23,34 @@ const numero=document.querySelector("#numero")
 
 //array dos textos saida
 const stringArray = []
+
+//variavel de controle
+var controle = 1
+
+console.log(coluna_um)
+
+//caracteres e simbolos - keyboard
+const c1  = ["a","b","c","d","e","f","g","h","i","j"]
+const caracteresC1 = [1,2,3,4,5,6,7,8,9,0]
+const c2 = ["k","l","m","n","o","p","q","r","s","."]
+const caracteresC2 = ["@","#","$","_","&","-","+","(",")","/"]
+const c3 = ["u","v","w","x","y","z","_","-",";",","]
+const caracteresC3 = ["*","'",".",":",";",">","<","+","-"," \ "]
+
+numero.addEventListener("click",()=>{
+    numero.classList.toggle("numero_ativo")
+    ++controle
+    if(controle%2==0){
+        coluna_um.map((e,ind)=>e.innerHTML = caracteresC1[ind])
+        coluna_dois.map((e,ind)=>e.innerHTML = caracteresC2[ind])
+        coluna_tres.map((e,ind)=>e.innerHTML = caracteresC3[ind])
+    }else{
+        coluna_um.map((e,ind)=>e.innerHTML=c1[ind])
+        coluna_dois.map((e,ind)=>e.innerHTML=c2[ind])
+        coluna_tres.map((e,ind)=>e.innerHTML=c3[ind])
+    }
+})
+
 
 //variavel de controle upper
 var controleUpper = false
@@ -75,3 +106,10 @@ const entrada=()=>{
     })
 }
 entrada()
+
+
+function createRecognetion(){
+    const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecogtion
+    const recognition = SpeechRecognition != undefined 
+}
+createRecognetion()
